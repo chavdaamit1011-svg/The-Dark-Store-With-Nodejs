79addc893e2e3c65b1411688462992e151699d80
Chavda Amit <chavdaamit1011@gmail.com>
2026-08-12 14:36:09 +0530

    Delete NodeJsProject directory

--- NodeJsProject/Backeend/testApi.js
+++
@@ -1,5 +0,0 @@
-fetch("http://localhost:8024/forgot-password", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ email: "chavdaamit1011@gmail.com" })
-}).then(res => res.json()).then(data => console.log("Response:", data)).catch(err => console.log("Error:", err));
--- NodeJsProject/Frontend/Api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const Api = axios.create({
-    baseURL: import.meta.env.MODE === 'development' ? "http://localhost:8024" : "" 
-})
-
-export default Api
